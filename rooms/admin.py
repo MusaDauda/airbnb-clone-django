52228c474d25c1b4a9d3e2dbd3437c45d8aea901
Musa Dauda <marshalmusa143@gmail.com>
2022-07-08 22:52:24 +0100

    added more abstract class that maps m2m r/ship with rooms: learned about ondelete

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.RoomType)
+@admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
 
     pass
